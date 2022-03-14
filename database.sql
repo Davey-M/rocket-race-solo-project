894bf3cf8d977301999adc5e039084e2f77be041
Davey-M <davey.meuer@gmail.com>
2022-03-14 13:44:45 -0500

    add initial database

--- database.sql
+++ database.sql
@@ -1,4 +1,3 @@
-
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
@@ -6,5 +5,21 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "about" TEXT,
+    "img" TEXT,
+    "access_level" INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE "race" (
+  "id" SERIAL PRIMARY KEY,
+  "time" TIMESTAMPTZ,
+  "winner_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "users_races" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"race_id" INT REFERENCES "race",
+	"finish_time" FLOAT
 );
