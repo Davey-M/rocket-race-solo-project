971f38b2207145af6b0b1fb19fd08b5056d9d97d
Davey-M <davey.meuer@gmail.com>
2022-03-17 11:01:06 -0500

    add get profile router

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -3,8 +3,27 @@ const router = express.Router();
 
 const pool = require('../modules/pool');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  if (req.isAuthenticated()) {
+    try {
+      const id = req.body.id;
 
+      const sqlText = `
+        SELECT "id", "username", "about", "img" FROM "user" WHERE "id" = $1;
+      `
+
+      const sqlOptions = [id];
+
+      const response = await pool.query(sqlText, sqlOptions);
+
+      res.send(response.rows);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 
 module.exports = router;
