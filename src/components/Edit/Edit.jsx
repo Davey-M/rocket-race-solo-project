baa25c6aa0f8af75c48e6ee0f48ac68b36e3c74f
Davey-M <davey.meuer@gmail.com>
2022-03-18 09:56:48 -0500

    style edit page

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -1,9 +1,36 @@
 import './Edit.css';
 
 function Edit() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('submitting');
+  };
+
   return (
     <div className='edit-container'>
       <h2 className='dark-back edit-header'>Edit Profile</h2>
+      <form onSubmit={handleSubmit} className='edit-form'>
+        <div>
+          <p>Username:</p>
+          <input type='text' placeholder='Username' />
+        </div>
+        <div>
+          <p>Image URL:</p>
+          <input type='text' placeholder='Image URL' />
+        </div>
+        <div className='long-section'>
+          <p>About Me:</p>
+          {/* <input type='text' placeholder='About' /> */}
+          <textarea name='' id='' cols='30' rows='3'></textarea>
+        </div>
+        <div className='button-container'>
+          <button type='submit'>Save</button>
+          <button className='free red' type='button'>
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
