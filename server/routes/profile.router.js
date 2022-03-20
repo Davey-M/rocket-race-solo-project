87e7ffeb0d51983b4858d4a678cb7addb707c762
Davey-M <davey.meuer@gmail.com>
2022-03-19 19:51:41 -0500

    get card_ids with sql

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -9,7 +9,14 @@ router.get('/:id', async (req, res) => {
       const id = req.params.id;
 
       const sqlText = `
-        SELECT "user"."id", "username", "about", "img", array_agg("users_races"."finish_time") AS "finish_time", array_agg("users_races"."place") AS "place", array_agg("users_races"."race_id") AS "race_id" FROM "user"
+        SELECT
+          "user"."id", "username",
+          "about", "img",
+          array_agg("users_races"."finish_time") AS "finish_time",
+          array_agg("users_races"."place") AS "place",
+          array_agg("users_races"."race_id") AS "race_id",
+          array_agg("users_races"."id") AS "card_id"
+        FROM "user"
         FULL JOIN "users_races"
           ON "user"."id" = "users_races"."user_id"
         WHERE "user"."id" = $1
