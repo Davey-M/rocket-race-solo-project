0c93e3d3e8ae86d81d3d6732df7a9c994abdf284
Davey-M <davey.meuer@gmail.com>
2022-03-21 14:48:49 -0500

    wip race game new

---
+++ server/modules/socket.handler.js
@@ -0,0 +1,36 @@
+let game = [];
+
+function socketHandler(socket, io) {
+  console.log(socket.id);
+
+  socket.join('room');
+
+  game.push({
+    id: socket.id,
+    x: 200,
+    y: 200,
+    rotation: 0,
+  });
+
+  socket.conn.on('close', () => {
+    game = game.filter((s) => s.id !== socket.id);
+  })
+
+  socket.on('move', (shipState) => {
+
+    game.forEach((ship, index) => {
+      if (ship.id === shipState.id) {
+        ship = {
+          ...ship,
+          ...shipState,
+        }
+      }
+    })
+
+    io.emit('ship-move', game)
+  })
+}
+
+
+
+module.exports = socketHandler;
--- src/components/Race/Race.jsx
+++ src/components/Race/Race.jsx
@@ -1,15 +1,89 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import './Race.css';
 
 function Race() {
+  const dispatch = useDispatch();
+
   const socket = useSelector((store) => store.socket);
   const game = useSelector((store) => store.game);
 
-  useEffect(() => {}, []);
+  const player = {
+    x: 200,
+    y: 200,
+    rotation: 0,
+  };
+  const playerSpeed = 100;
+  const turningSpeed = 10;
+
+  // socket setup useEffect
+  useEffect(() => {
+    socket?.on('message', (m) => {
+      console.log('message', m);
+    });
+
+    socket?.on('ship-move', (gameState) => {
+      dispatch({
+        type: 'SET_GAME',
+        payload: gameState,
+      });
+    });
+
+    return () => {
+      socket?.removeAllListeners('message');
+      socket?.removeAllListeners('ship-move');
+    };
+  }, [socket]);
+
+  // keypress listener
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeys);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeys);
+    };
+  }, []);
+
+  function handleKeys(e) {
+    // console.log(e.key);
+
+    switch (e.key) {
+      case 'ArrowUp':
+        player.x = Math.sin(player.rotation) * player.x;
+        return;
+      default:
+        return;
+    }
+  }
 
   return (
     <>
-      <div className='game-board'></div>
+      {socket && (
+        <div className='game-board'>
+          <div
+            className='ship me'
+            style={{
+              marginTop: player.x,
+              marginLeft: player.y,
+              transform: `rotate(${player.rotation}deg)`,
+            }}
+          ></div>
+          {game?.map((racer) => {
+            if (racer.id === socket.id) return '';
+            return (
+              <div
+                className='ship'
+                style={{
+                  x: racer.x,
+                  y: racer.y,
+                  transform: `rotate(${racer.rotation}deg)`,
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
