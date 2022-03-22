9f6b436fe9914c6799cd0d5e7ba2b990d750b33e
Davey-M <davey.meuer@gmail.com>
2022-03-22 10:01:32 -0500

    added check function to asteroids

--- src/components/Asteroid/Asteroid.jsx
+++ src/components/Asteroid/Asteroid.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Asteroid({ asteroid, startingTop }) {
+  const dispatch = useDispatch();
+
   let startingPos = asteroid[5];
   let movements = asteroid.filter((a, index) => index !== 5);
 
   const [position, setPosition] = useState(startingPos);
   const [step, setStep] = useState(0);
+
+  const player = useSelector((store) => store.player);
 
   useEffect(() => {
     let interval = setInterval(() => {
@@ -42,3 +47,12 @@ function Asteroid({ asteroid, startingTop }) {
 }
 
 export default Asteroid;
+
+function checkPosition(player, asteroid) {
+  return (
+    player.x > asteroid.x &&
+    player.x < asteroid.x + 50 &&
+    2000 - player.y > asteroid.y &&
+    2000 - player.y < asteroid.y + 50
+  );
+}
