8e7070be607dd228d84a68fade8692382e3d6db2
Davey-M <davey.meuer@gmail.com>
2022-03-22 11:11:29 -0500

    working movement

--- src/components/Race/Race.jsx
+++ src/components/Race/Race.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import player from '../../redux/reducers/player.reducer';
 
 import './Race.css';
 
@@ -15,7 +16,8 @@ function Race() {
   return (
     <>
       <div className='game-board-container'>
-        <canvas ref={gameBoard} className='game-board'></canvas>
+        {/* <canvas ref={gameBoard} className='game-board'></canvas> */}
+        <div ref={gameBoard} className='game-board'></div>
       </div>
     </>
   );
@@ -27,29 +29,102 @@ function main(socket, gameBoard) {
   // exit if socket does not exist
   if (!socket || !gameBoard) return;
 
-  const canvas = gameBoard.current;
-  const context = canvas.getContext('2d');
+  // const canvas = gameBoard.current;
+  const board = gameBoard.current;
+  // const context = canvas.getContext('2d');
 
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
+  const blueRocket = document.createElement('img');
+  blueRocket.src = './blue-ship.PNG';
+  blueRocket.height = 50;
 
-  let square = { x: 0, y: 0 };
+  const playerShip = document.createElement('div');
+  playerShip.classList.add('ship');
+  playerShip.appendChild(blueRocket);
 
-  console.log({ canvas, context });
+  const stars = document.createElement('div');
+  stars.classList.add('game-board-stars');
+  board.appendChild(stars);
+
+  board.appendChild(playerShip);
+
+  // canvas.width = canvas.clientWidth;
+  // canvas.height = canvas.clientHeight;
+
+  let player = { x: 200, y: 1950, rotation: 0 };
+  const rocketSpeed = 50;
+  const rotationSpeed = 45;
+
+  // console.log({ canvas, context });
 
   function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    // context.clearRect(0, 0, canvas.width, canvas.height);
 
-    context.fillStyle = 'red';
-    context.fillRect(square.x, square.y, 50, 50);
+    // context.fillStyle = 'red';
+    // // context.fillRect(square.x, square.y, 50, 50);
+    // context.drawImage(blueRocket, square.x, square.y, 30, 50);
 
-    window.requestAnimationFrame(draw);
+    playerShip.style.marginLeft = `${player.x}px`;
+    playerShip.style.marginTop = `${player.y}px`;
+    playerShip.style.transform = `rotate(${player.rotation}deg)`;
+
+    if (player.y < 1600) {
+      board.style.marginBottom = `${-1 * (1600 - player.y)}px`;
+      stars.style.marginBottom = `${(-1 * player.y) / 2}px`;
+    } else {
+      board.style.marginBottom = `0px`;
+      stars.style.marginBottom = '-800px';
+    }
+
+    // window.requestAnimationFrame(draw);
   }
   draw();
 
-  setInterval(() => {
-    square.y += 50;
-  }, 500);
+  // remove the event listener when we leave the page
+  window.addEventListener('hashchange', () => {
+    window.removeEventListener('keydown', handleTurn);
+    clearInterval(moveInterval);
+  });
+
+  window.addEventListener('keydown', handleTurn);
+
+  let moveInterval = setInterval(() => {
+    player.x = Math.sin(player.rotation * 0.0174533) * rocketSpeed + player.x;
+    player.y =
+      Math.cos((player.rotation + 180) * 0.0174533) * rocketSpeed + player.y;
+
+    if (player.x > 700 || player.x < 0 || player.y > 2000) {
+      player.y = 1950;
+      player.x = 200;
+      player.rotation = 0;
+    }
+
+    draw();
+  }, 400);
+
+  function handleTurn(e) {
+    switch (e.key) {
+      case 'ArrowRight':
+        player.rotation += rotationSpeed;
+        break;
+      case 'ArrowLeft':
+        player.rotation -= rotationSpeed;
+        break;
+      case 'd':
+        player.rotation += rotationSpeed;
+        break;
+      case 'a':
+        player.rotation -= rotationSpeed;
+        break;
+      default:
+        break;
+    }
+
+    draw();
+  }
+
+  // setInterval(() => {
+  //   square.y -= 50;
+  // }, 500);
 
   function logic() {}
 }
