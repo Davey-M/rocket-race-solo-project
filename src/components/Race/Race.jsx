85a58f8d4dd19ed157540d9c245c49357c85d97e
Davey-M <davey.meuer@gmail.com>
2022-03-22 10:17:08 -0500

    create canvas

--- src/components/Race/Race.jsx
+++ src/components/Race/Race.jsx
@@ -1,248 +1,55 @@
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 
 import './Race.css';
 
-// ship image imports
-import blueShip from './blue-ship.png';
-import redShip from './red-ship.png';
-
-// component imports
-import Asteroid from '../Asteroid/Asteroid';
-
 function Race() {
-  const dispatch = useDispatch();
+  const gameBoard = useRef();
 
   const socket = useSelector((store) => store.socket);
-  const game = useSelector((store) => store.game);
-
-  const [started, setStarted] = useState(false);
-  const [asteroids, setAsteroids] = useState(null);
 
   useEffect(() => {
-    // when this component unmounts leave the game
-    return () => {
-      socket?.emit('leave');
-    };
-  }, []);
-
-  // const [player, setPlayer] = useState({
-  //   x: 200,
-  //   y: 1950,
-  //   rotation: 0,
-  // });
-  const player = useSelector((store) => store.player);
-
-  const playerSpeed = 50;
-  const turningSpeed = 45;
-
-  // socket setup useEffect
-  useEffect(() => {
-    socket?.on('message', (m) => {
-      console.log('message', m);
-    });
-
-    socket?.on('ship-move', (gameState) => {
-      dispatch({
-        type: 'SET_GAME',
-        payload: gameState,
-      });
-    });
-
-    socket?.on('game-started', (gameState) => {
-      dispatch({
-        type: 'SET_GAME',
-        payload: gameState,
-      });
-
-      setStarted(true);
-    });
-
-    socket.on('game-joined', (gameState) => {
-      dispatch({
-        type: 'SET_GAME',
-        payload: gameState,
-      });
-
-      setAsteroids(gameState.asteroids);
-      setStarted(true); // test (should be removed)
-    });
-
-    // setPlayer({
-    //   ...player,
-    //   id: socket?.id,
-    // });
-
-    dispatch({
-      type: 'SET_PLAYER',
-      payload: {
-        ...player,
-        id: socket?.id,
-      },
-    });
-
-    return () => {
-      socket?.removeAllListeners('message');
-      socket?.removeAllListeners('ship-move');
-    };
+    main(socket, gameBoard);
   }, [socket]);
-
-  // keypress listener
-  useEffect(() => {
-    socket?.emit('move', player);
-
-    window.addEventListener('keydown', handleKeys);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeys);
-    };
-  }, [player]);
-
-  const handleKeys = (e) => {
-    // console.log(player);
-    switch (e.key) {
-      case 'ArrowUp':
-        // setPlayer({
-        //   ...player,
-        //   x: Math.sin(player.rotation * 0.0174533) * playerSpeed + player.x,
-        //   y:
-        //     Math.cos((player.rotation - 180) * 0.0174533) * playerSpeed +
-        //     player.y,
-        // });
-        dispatch({
-          type: 'SET_PLAYER',
-          payload: {
-            ...player,
-            x: Math.sin(player.rotation * 0.0174533) * playerSpeed + player.x,
-            y:
-              Math.cos((player.rotation - 180) * 0.0174533) * playerSpeed +
-              player.y,
-          },
-        });
-        // playerLocal.x = Math.sin(player.rotation) * playerSpeed + player.x;
-        // playerLocal.y = Math.cos(player.rotation) * playerSpeed + player.y;
-        break;
-      case 'ArrowRight':
-        // setPlayer({
-        //   ...player,
-        // });
-
-        dispatch({
-          type: 'SET_PLAYER',
-          payload: {
-            ...player,
-            rotation: player.rotation + turningSpeed,
-          },
-        });
-        break;
-      case 'ArrowLeft':
-        // setPlayer({
-        //   ...player,
-        //   rotation: player.rotation - turningSpeed,
-        // });
-
-        dispatch({
-          type: 'SET_PLAYER',
-          payload: {
-            ...player,
-            rotation: player.rotation - turningSpeed,
-          },
-        });
-        break;
-      default:
-        break;
-    }
-  };
-
-  const createGame = () => {
-    socket?.emit('create-game');
-  };
-
-  const startGame = () => {
-    socket?.emit('start-game');
-  };
-
-  const joinGame = () => {
-    socket?.emit('join-game' /* put the game code here */);
-  };
 
   return (
     <>
-      {socket && (
-        <>
-          {!started ? (
-            <div>
-              <div>
-                <button onClick={createGame}>Create Game</button>
-                <div>
-                  <input type='text' placeholder='Enter Game Code' />
-                  <button>Join Game</button>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div className='game-board-container'>
-              <div
-                className='game-board'
-                style={{
-                  marginBottom: player.y < 1600 ? -1 * (1600 - player.y) : 0,
-                }}
-              >
-                {/* stars in the background */}
-                <div
-                  className='game-board-stars'
-                  style={{
-                    marginBottom:
-                      player.y < 1600 ? (-1 * player.y) / 2 : (-1 * 1600) / 2,
-                  }}
-                ></div>
-
-                {/* other client players */}
-                {game?.players.map((racer, index) => {
-                  if (racer.id === socket.id) return '';
-                  console.log(racer);
-                  return (
-                    <div
-                      key={index}
-                      className='ship'
-                      style={{
-                        marginLeft: racer.x + 'px',
-                        marginTop: racer.y + 'px',
-                        transform: `rotate(${racer.rotation}deg)`,
-                      }}
-                    >
-                      <img src={redShip} alt='' />
-                    </div>
-                  );
-                })}
-
-                {/* this clients player */}
-                <div
-                  className='ship me'
-                  style={{
-                    marginLeft: player.x + 'px',
-                    marginTop: player.y + 'px',
-                    transform: `rotate(${player.rotation}deg)`,
-                  }}
-                >
-                  <img src={blueShip} alt='' />
-                </div>
-
-                {/* asteroids */}
-                {asteroids?.map((asteroid, index) => (
-                  <Asteroid
-                    key={index}
-                    asteroid={asteroid}
-                    startingTop={index * 150}
-                  />
-                ))}
-              </div>
-            </div>
-          )}
-        </>
-      )}
+      <div className='game-board-container'>
+        <canvas ref={gameBoard} className='game-board'></canvas>
+      </div>
     </>
   );
 }
 
 export default Race;
+
+function main(socket, gameBoard) {
+  // exit if socket does not exist
+  if (!socket || !gameBoard) return;
+
+  const canvas = gameBoard.current;
+  const context = canvas.getContext('2d');
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+
+  let square = { x: 0, y: 0 };
+
+  console.log({ canvas, context });
+
+  function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = 'red';
+    context.fillRect(square.x, square.y, 50, 50);
+
+    window.requestAnimationFrame(draw);
+  }
+  draw();
+
+  setInterval(() => {
+    square.y += 50;
+  }, 500);
+
+  function logic() {}
+}
