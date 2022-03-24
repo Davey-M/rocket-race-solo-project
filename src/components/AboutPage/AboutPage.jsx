d27d1e323c6d9982d2bd20b7921b55e9de2cda7c
Davey-M <davey.meuer@gmail.com>
2022-03-24 10:37:37 -0500

    add storyline to game

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './AboutPage.css';
 
+import crowdedImg from './Crowded.jpg';
+import raceImg from './Race.jpg';
+import peaceImg from './Peace.jpg';
+
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -13,12 +17,23 @@ function AboutPage() {
       <section>
         <h1 className='blue-back about-header'>The Story</h1>
         <div className='content'>
-          <p>
-            You are an astronaut whose planet is becoming too crowded. You have
-            to get out of here and find a new planet. The only problem is that
-            you are not the only astronaut with this plan. Be the first to get
-            to a new planet and you can have it all to yourself. Good luck!
-          </p>
+          <div className='story-grid'>
+            <p>
+              You are an astronaut whose planet is becoming too crowded. You
+              have to get out of here and find a new planet.
+            </p>
+            <img src={crowdedImg} alt='' />
+            <img src={raceImg} alt='' />
+            <p>
+              The only problem is that you are not the only astronaut with this
+              plan.
+            </p>
+            <p>
+              Be the first to get to a new planet and you can have it all to
+              yourself. Good luck!
+            </p>
+            <img src={peaceImg} alt='' />
+          </div>
         </div>
       </section>
       <section>
